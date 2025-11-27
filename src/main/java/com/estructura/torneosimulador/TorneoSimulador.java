f20f1f2ad1c4296261115ad372844879f00eca6a
Ing M Ortiz <174818641+mortiza1@users.noreply.github.com>
2025-11-27 00:10:34 -0500

    feat: primer commit del proyecto TorneoSimulador

---
+++ src/main/java/com/estructura/torneosimulador/TorneoSimulador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.estructura.torneosimulador;
+
+/**
+ *
+ * @author MOA
+ */
+public class TorneoSimulador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
